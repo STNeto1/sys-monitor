2364e06f93d208a13dcac3777f79fa686bed6d83
STNeto1 <gfreitasneto18@gmail.com>
2023-04-05 23:29:57 -0300

    10s sleep loop

--- monitor-agent/src/main.rs
+++ monitor-agent/src/main.rs
@@ -1,18 +1,25 @@
+use std::{thread, time::Duration};
+
 use monitor_central::Stat;
 use sysinfo::{System, SystemExt};
 
 fn main() {
     let mut sys = System::new_all();
-    sys.refresh_all();
 
-    let stat_data = Stat {
-        system_identifier: sys.host_name().unwrap_or("unknown".to_string()),
-        total_memory: sys.total_memory(),
-        used_memory: sys.used_memory(),
-        total_swap: sys.total_swap(),
-        used_swap: sys.used_swap(),
-        uptime: sys.uptime(),
-    };
+    loop {
+        sys.refresh_all();
 
-    println!("{:?}", stat_data);
+        let stat_data = Stat {
+            system_identifier: sys.host_name().unwrap_or("unknown".to_string()),
+            total_memory: sys.total_memory(),
+            used_memory: sys.used_memory(),
+            total_swap: sys.total_swap(),
+            used_swap: sys.used_swap(),
+            uptime: sys.uptime(),
+        };
+
+        println!("{:?}", stat_data);
+
+        thread::sleep(Duration::from_secs(10));
+    }
 }
